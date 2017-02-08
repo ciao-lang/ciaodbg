0161231b83b64fa591995a9d2ee1cbec3919aa57
Jose F. Morales <jfmcjf@gmail.com>
2017-02-08 18:10:28 +0100

    Public snapshot for a732dce7cb2d0117c428b4645d664dfd2e4f6682
    Src-commit: a732dce7cb2d0117c428b4645d664dfd2e4f6682

--- lib/unittest/unittest_examples.pl
+++ lib/unittest/unittest_examples.pl
@@ -11,43 +11,43 @@
 @section{test_examples.pl}
 
 @begin{verbatim}
-@includeverbatim{unittest/examples/test_examples.pl}
+@includeverbatim{examples/test_examples.pl}
 @end{verbatim}
 
 @section{ceval1.pl}
 
 @begin{verbatim}
-@includeverbatim{unittest/examples/ceval1.pl}
+@includeverbatim{examples/ceval1.pl}
 @end{verbatim}
 
 @section{ceval2.pl}
 
 @begin{verbatim}
-@includeverbatim{unittest/examples/ceval2.pl}
+@includeverbatim{examples/ceval2.pl}
 @end{verbatim}
 
 @section{length.pl}
 
 @begin{verbatim}
-@includeverbatim{unittest/examples/length.pl}
+@includeverbatim{examples/length.pl}
 @end{verbatim}o
 
 @section{civil_registry.pl}
 
 @begin{verbatim}
-@includeverbatim{unittest/examples/civil_registry.pl}
+@includeverbatim{examples/civil_registry.pl}
 @end{verbatim}
 
 @section{qsort.pl}
 
 @begin{verbatim}
-@includeverbatim{unittest/examples/qsort.pl}
+@includeverbatim{examples/qsort.pl}
 @end{verbatim}
 
 @section{relationships.pl}
 
 @begin{verbatim}
-@includeverbatim{unittest/examples/relationships.pl}
+@includeverbatim{examples/relationships.pl}
 @end{verbatim}
 
 ").
