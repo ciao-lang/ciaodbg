e194c0a3333c35799b7727a68e7ccf4f4b12d165
Jose F. Morales <jfmcjf@gmail.com>
2019-04-16 11:32:49 +0200

    Do not use "foreign_inline" (currently the code is very hard to read)
    Src-commit: 6dffe8cc319177f4244b14d4dc884cee9dfe1bcd

--- lib/hashtable/hashtable.pl
+++ lib/hashtable/hashtable.pl
@@ -4,11 +4,7 @@
 :- use_foreign_source(library(hashtable/recycle)).
 :- use_foreign_source(library(hashtable/lookupa)).
 :- use_foreign_source(library(hashtable/hashtab)).
-
-:- foreign_inline("
-#include <string.h>
-#include \"hashtab.h\"
-").
+:- use_foreign_source(library(hashtable/hashtable)).
 
 :- trust pred ht_create(in(LogSize), go(HTab)) :: c_long * address + (foreign, returns(HTab)).
 
@@ -17,45 +13,17 @@
 :- trust pred ht_count(in(HTab), go(Count)) :: address * c_long + (foreign(ht_count_),
    returns(Count)).
 
-:- foreign_inline(ht_count/2,
-"long ht_count_(ht_tab *t)
-{
-  return ht_count(t);
-}
-").
-
 % NOTE: Keys are stored as arrays of chars, but they are not \0-ended
 % (so we cannot translate easily to atoms)
 
 :- trust pred ht_key(in(HTab), go(Key)) :: address * address +
    (foreign(ht_key_), returns(Key)).
 
-:- foreign_inline(ht_key/2,
-"void *ht_key_(ht_tab *t)
-{
-  return ht_key(t);
-}
-").
-
 :- trust pred ht_keyl(in(HTab), go(Keyl)) :: address * c_long +
    (foreign(ht_keyl_), returns(Keyl)).
 
-:- foreign_inline(hkeyl_/2,
-"long ht_keyl_(ht_tab *t)
-{
-  return ht_keyl(t);
-}
-").
-
 :- trust pred ht_stuff(in(HTab), go(Stuff)) :: address * address +
    (foreign(ht_stuff_), returns(Stuff)).
-
-:- foreign_inline(ht_stuff/2,
-"void * ht_stuff_(ht_tab *t)
-{
-  return ht_stuff(t);
-}
-").
 
 % TODO: Missing predicates for (address,length)<->atom conversion
 
@@ -68,12 +36,6 @@
 :- trust pred ht_add2(in(HTab), in(Key), in(Stuff), go(Add)) ::
    address * atm * address * c_long + (foreign, returns(Add)).
 
-:- foreign_inline(ht_add2/4,
-"long ht_add2(ht_tab *t, char *key, void *stuff) {
-  return ht_add(t, (ub1*)key, strlen((char *)key), stuff);
-}
-").
-
 :- trust pred ht_del(in(HTab), go(Del)) :: address * c_long + (foreign,
    returns(Del)).
 
@@ -82,13 +44,6 @@
 
 :- trust pred ht_next(in(HTab), go(Next)) :: address * c_long + (foreign(ht_next_),
    returns(Next)).
-
-:- foreign_inline(ht_next/2,
-"long ht_next_(ht_tab *t)
-{
-  return ht_next(t);
-}
-").
 
 :- trust pred ht_nbucket(in(HTab), go(NBucket)) :: address * c_long +
    (foreign, returns(NBucket)).
