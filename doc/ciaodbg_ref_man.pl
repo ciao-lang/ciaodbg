801f7fbce5239aa35b35db14e57b81a468276b1c
Manuel Hermenegildo <manuel.hermenegildo@imdea.org>
2023-10-14 16:58:18 +0200

    (ciaodbg) Some improvements to bundle intro
    Src-commit: f371d18bc6510e327989d7b6071997b335a8caf0

--- doc/ciaodbg_ref_man.pl
+++ doc/ciaodbg_ref_man.pl
@@ -2,23 +2,26 @@
 
 :- doc(filetype, application).
 
+% The (main) debugger is not here; also, 'for Ciao' is redundant. Title changed. 
 %:- doc(title, "Debugging, Testing and Profiling for Ciao").
-:- doc(title, "Run-time Checking, Testing, and Profiling for Ciao").
+:- doc(title, "Run-time Checking, Testing, and Profiling").
+
 :- doc(bug, "add in the main manual a reference for rtchecks documentation here.").
 :- doc(bug, "move debugger here (as chapter), THEN put debugger in title.").
 
 % :- include(core_docsrc(common/'ClipAddress')).
 
-:- doc(summary, "This bundle implements a debugging, testing, and
-profiling framework (aka
-@href{https://en.wikipedia.org/wiki/Dynamic_program_analysis}{dynamic
-program analysis}) for Ciao.
+:- doc(summary, "This bundle contains some of the run-time checking,
+testing, and profiling facilities of the Ciao program development
+framework.
 
-As other components in Ciao (like documentation generator and static
-analyzer), this framework makes use of program assertions to obtain
-the specification of desired behaviors (e.g., test assertions).
+Similarly to other components, like the static analyzer or
+the documentation generator, these parts of the framework make use of
+program assertions to obtain the specification of desired behaviors
+(this includes test assertions and the assertions from which run-time
+tests are generated).
 
-It is integrated into the Emacs-based development environment (under
-the @tt{CiaoDbg} menu) and the Ciao builder (@tt{ciao test}
-command). See reference manual for more information.  ").
+Some of these facilities are integrated into the Ciao builder
+(@tt{ciao test} command) and the different development environments
+(e.g., in Emacs --under the @tt{CiaoDbg} menu--, Playground, etc.). ").
 
